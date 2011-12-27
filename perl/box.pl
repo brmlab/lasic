c7f9e941601089ee75266094469fcdb90e4e81fb
Petr Baudis <pasky@ucw.cz>
2011-12-27 23:02:22 +0100

    box.pl: Add progress prints

--- perl/box.pl
+++ perl/box.pl
@@ -9,14 +9,23 @@ use lib qw(.);
 use Brm::Lasic;
 
 my $lasic = Brm::Lasic->new(dev => '/dev/ttyUSB0');
+print "reset\n";
 $lasic->reset();
+sleep 1;
 
+print "focus -2\n";
 $lasic->focus(-2);
+sleep 1;
+print "focus 2\n";
 $lasic->focus(2);
+sleep 1;
 
+print "laser on\n";
 $lasic->laser_on();
+sleep 1;
 $lasic->move(100, 0);
+$lasic->move(100, 100);
 $lasic->move(0, 100);
-$lasic->move(-100, 0);
-$lasic->move(0, -100);
+$lasic->move(0, 0);
 $lasic->laser_off();
+print "laser off\n";
